fa442e3bce61982ba23db2c3611a9fca9e3533ba
Henry Blanchette <henry@monkeyface.com>
2024-11-05 23:05:13 -0500

    made keys private (and disabled old key)

--- ai.py
+++ ai.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
 
+import keys
+
 client = OpenAI(
     organization="org-6MzLQ9ZEmqSZHH6L1DZhFP4k",
     project="proj_j5qzsNihWPBmUVrCKrQumQTk",
-    api_key="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",
+    api_key=keys.OPENAI_API_KEY,
 )
